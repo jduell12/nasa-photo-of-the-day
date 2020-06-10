f21691536918311924a2c5080491ee39dc4f24ca
Jessica <duellje@oregonstate.edu>
2020-06-10 15:20:24 -0400

    getting api data from nasa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import {BASE_URL, API_KEY} from './constants/index'
 import "./App.css";
 
 function App() {
+  const [nasaData, changeData] = useState([]);
+
+  useEffect(()=>{
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        changeData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+      return ()=>{}
+  }, [])
+
   return (
     <div className="App">
       <p>
