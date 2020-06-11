3b81dc4731f1cee33d917ff1dfae223556bb5700
Jessica <duellje@oregonstate.edu>
2020-06-11 16:56:33 -0400

    used styled components on explanation paragraph

--- src/components/Explanation.jsx
+++ src/components/Explanation.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledP = styled.p`
+    color: #FEBA67;
+    text-align: justify;
+    font-family: 'Noto Sans JP', sans-serif;
+    padding: 3%;
+`
 
 export default function Explanation (props){
     const {explanation} = props;
     return (
         <div>
-            <p>{explanation}</p>
+            <StyledP>{explanation}</StyledP>
         </div>
     )
 }
