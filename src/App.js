b57770fe20839336bc24329dd2460686befc4477
Jessica <duellje@oregonstate.edu>
2020-06-12 10:49:26 -0400

    removed code that was no longer needed

--- src/App.js
+++ src/App.js
@@ -28,18 +28,7 @@ function App() {
 
       return ()=>{}
   }, [])
-
-
-  // <AppStyled className="App">
-  //     <Title title = 'Good Title' date = 'June 11, 2020'/>
-  //     <br></br>
-  //     <Picture url = 'https://images.unsplash.com/photo-1543722530-d2c3201371e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2853&q=80'/>
-  //     <Explanation explanation = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
-  //     <Footer copyright = 'Author'/>
-  //   </AppStyled>
-    
   
-
   return (
     <AppStyled className="App">
       <Title title = {nasaData.title} date = {nasaData.date}/>
